0e5490225c27df0b0ce35508ae7746cefd770c2a
Filipp Semirov <filipp.semirov@umbrellait.com>
2019-01-24 16:20:37 +0300

    add initial

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+        <header>
+          <p>
+            header
+          </p>
+        </header>
+    );
+  }
+}
+
+export default Header;
